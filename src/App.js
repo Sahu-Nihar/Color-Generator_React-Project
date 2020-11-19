7807c99c9640cd2c4c19ab4622d1e7d25b2a5529
Nihar Sahu <sahunihar0602@gmail.com>
2020-11-19 21:41:56 +0530

    Color-Generator React Project using Forms and State, Effect Hooks.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import SingleColor from './SingleColor'
+
+import Values from 'values.js'
 
 function App() {
+  const [color, setColor] = useState('');
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values('#00ff00').all(10));
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    try{
+      let colors = new Values(color).all(10);
+      setList(colors);
+    }catch(error) {
+      setError(true);
+      console.log(error);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={color} onChange={(event) => setColor(event.target.value)} placeholder="#fff" className={`${error? 'error': null}`}/>
+          <button className="btn">submit</button>
+        </form>
+      </section>
+      <section className="colors">
+        {
+          list.map((color, index) => {
+            return (
+              <SingleColor key={index} {...color} index={index} hexColor={color.hex}/>
+            );
+          })
+        }
+      </section>
+    </>
   );
 }
 
-export default App;
+export default App
